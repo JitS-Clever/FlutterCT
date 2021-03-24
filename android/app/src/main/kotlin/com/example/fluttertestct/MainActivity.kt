beeaf58de2eada12d5dbccaac38f574c22c333de
Jits-Clever <jitedra.sahoo@clevertap.com>
2021-03-24 10:35:55 +0530

    commit

---
+++ android/app/src/main/kotlin/com/example/fluttertestct/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.fluttertestct
+
+import io.flutter.embedding.android.FlutterActivity
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
+
+class MainActivity: FlutterActivity() {
+
+}
